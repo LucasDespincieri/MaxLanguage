9a9d0d0918bc42882ab1d32c7ce44e5c3c4f6d3b
LucasMiguelDespincieriSantos <2511600256@fema.edu.br>
2026-08-31 20:18:08 -0300

    Implementação do método de inserção de nó

--- model/ArvoreBinaria.py
+++ model/ArvoreBinaria.py
@@ -1,23 +1,41 @@
-
 class No:
-    def __init__(self, chave, posicao_arquivo):
-        self.chave = chave
+    def __init__(self, codigo, posicao_arquivo):
+        self.codigo = codigo
         self.posicao_arquivo = posicao_arquivo
         self.esquerda = None
         self.direita = None
+
 
 class ArvoreBinaria:
     def __init__(self):
         self.raiz = None
 
-    def inserir(self, chave, posicao_arquivo):
+    def inserir(self, codigo, posicao_arquivo):
 
+        novo = No(codigo, posicao_arquivo)
+
+        if self.raiz is None:
+            self.raiz = novo
+            return
+
+        noAtual = self.raiz
+        noPai = None
+
+        while noAtual is not None:
+            noPai = noAtual
+
+            if codigo < noPai.codigo:
+                noAtual = noPai.esquerda
+            else:
+                noAtual = noPai.direita
+
+            if codigo < noPai.codigo:
+                noPai.esquerda = noAtual
+            else:
+                noPai.direita = noAtual
+
+    def buscar(self, codigo):
         pass
 
-    def buscar(self, chave):
-
-        pass
-
-    def excluir(self, posicao_arquivo):
-
+    def excluir(self, codigo, posicao_arquivo):
         pass
