02654d6a8a272b9631cf39bc06ce38d72f1139d0
lucasmds <lmdespincieri@gmail.com>
2026-09-17 09:55:28 -0300

    Classe de exercicio finalizada

--- model/Exercicio.py
+++ model/Exercicio.py
@@ -1,5 +1,6 @@
 from unicodedata import digit
 
+from main import tabelLicao
 from model.ArvoreBinaria import ArvoreBinaria
 from model.Gerenciador import Gerenciador
 
@@ -33,4 +34,47 @@ class Exercicio:
 
     def adicionarExercicio(self, codigo, codLicao, nivelDificuldade,
                            tipo, descricao, opcoes, resposta, pontucao):
-        pass
+
+        if self.arvore.buscar(codigo) is not None:
+            print(f"O codigo inserido({codigo}) já existe na tabela de exercício")
+            return
+
+        licaoPertencente = self.tabelaLicao.buscarLicao(codLicao)
+
+        if licaoPertencente is None:
+            print(f"O codigo de licao inserido({codLicao}) não existe na tabela de Lição")
+            return
+
+        codIdioma = licaoPertencente['codIdioma']
+        idiomaEncontrado = self.tabelaIdioma.buscarIdioma(codIdioma)
+        nomeIdioma = idiomaEncontrado['nome'] if idiomaEncontrado else "Desconhecido"
+
+        string = (f"{codigo};{codLicao};{nivelDificuldade};{tipo};{descricao};{opcoes};"
+                  f"{resposta};{pontucao}")
+
+        posicao = self.arquivo.gravarRegistro(string)
+        self.arvore.inserir(codigo, posicao)
+
+        print(f"Exercício adicionado à lição '{licaoPertencente['titulo']}' do idioma {nomeIdioma}")
+
+    def buscarExercicio(self, codigo):
+        noEncontrado = self.arvore.buscar(codigo)
+
+        if noEncontrado is not None:
+            linha = self.arquivo.lerRegistro(noEncontrado.posicaoArquivo)
+            dados = linha.strip().split(';')
+
+            if len(dados) == 8:
+                lista_opcoes = dados[5].split('|')
+
+                return {
+                    "codigo": int(dados[0]),
+                    "cod_licao": int(dados[1]),
+                    "nivel_dificuldade": int(dados[2]),
+                    "tipo": int(dados[3]),
+                    "descricao": dados[4],
+                    "opcoes": lista_opcoes,
+                    "resposta_correta": dados[6],
+                    "pontuacao": int(dados[7])
+                }
+        return None
